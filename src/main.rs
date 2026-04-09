125a2f447863be90a500015ccfce9b1ff7f0bcc8
StudiCraft <boultareauloevan168@gmail.com>
2026-04-09 20:03:41 +0200

    Added base paga project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("-----paga (WIP)-----");
+    println!("The paga project is Work In Progress.");
+    println!("It is currently for learning purposes only.");
+    println!("It is going to be updated whenever I find time and learn something new that could help me add functionnalities to it.");
+    println!("-----THANKS IN ADVANCE-----");
 }
